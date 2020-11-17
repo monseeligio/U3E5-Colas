71b4aca9bdccc1555a4e1e865163f7cef274acf5
monseeligio <71795808+monseeligio@users.noreply.github.com>
2020-11-16 19:46:08 -0600

    Create Cola.java

---
+++ Cola.java
@@ -0,0 +1,41 @@
+package colas;
+public class Cola{
+    Nodo cima;
+    int tamanio;
+    Nodo raiz;
+        public boolean vacia(){
+        return cima==null;
+    }
+    
+    public void empuja(int elemento){
+
+        Nodo nodo= new Nodo(elemento);
+         if(raiz==null && cima==null){
+         raiz=nodo;
+         cima=nodo;
+        }
+        cima.Siguiente=nodo;
+        cima=nodo;
+        tamanio++;
+        
+    }
+    
+    public void mostrar(){
+      if(vacia()){
+            System.out.println("La pila esta vacia");
+        }
+        else{
+        Nodo recorre=raiz;
+        while(recorre!=null){
+            System.out.println(recorre.elemento);
+            recorre=recorre.Siguiente;}
+        }
+    }
+    
+
+    public void eliminar(){
+       raiz=raiz.Siguiente;
+       tamanio--;
+    }
+    
+}
