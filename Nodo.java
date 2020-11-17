1d6d172c084d7bea76b614ab838d505659367c05
monseeligio <71795808+monseeligio@users.noreply.github.com>
2020-11-16 19:46:40 -0600

    Create Nodo.java

---
+++ Nodo.java
@@ -0,0 +1,13 @@
+
+package colas;
+
+public class Nodo {
+     int elemento;
+    Nodo Siguiente;
+    
+    
+    public Nodo (int elemento){
+        this.elemento=elemento;
+        Siguiente=null; 
+}
+}
